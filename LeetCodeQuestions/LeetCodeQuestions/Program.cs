9c477c22cd257ff880874a049c25b07330765366
Rashmi Singh <rassingh@microsoft.com>
2022-03-24 06:10:33 +0530

    committing first successful submission for today

---
+++ LeetCodeQuestions/LeetCodeQuestions/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCodeQuestions;
+
+Console.WriteLine("Hello, World!");
+var reverseVowels = new ReverseVowelsOfString();
+var result = reverseVowels.ReverseVowels("a.");
+Console.WriteLine(result);
+Console.ReadLine();
---
+++ LeetCodeQuestions/LeetCodeQuestions/ReverseVowelsOfString.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCodeQuestions
+{
+    using System;
+    using System.Linq;
+
+    public class ReverseVowelsOfString
+    {
+        public string ReverseVowels(string s)
+        {
+            var vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            var charArray = s.ToCharArray();
+            int lastSwapped = charArray.Length;
+            var result = string.Empty;
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (vowels.Contains(charArray[i]))
+                {
+                    if (i >= lastSwapped)
+                    {
+                        break;
+                    }
+
+                    var index = FindVowelFromLeft(charArray, i, lastSwapped - 1, vowels);
+                    if (index < 0)
+                    {
+                        break;
+                    }
+
+                    lastSwapped = index;
+                    var temp = charArray[i];
+                    charArray[i] = charArray[index];
+                    charArray[index] = temp;
+                }
+            }
+
+            return new string(charArray); ;
+        }
+
+        public int FindVowelFromLeft(char[] arr, int start, int end, char[] vowels)
+        {
+            for (int i = end; i > start; i--)
+            {
+                if (vowels.Contains(arr[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
