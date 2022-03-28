12322fa19847da1d844be5b95f246f5ac5bcfe52
Rashmi Singh <rassingh@microsoft.com>
2022-03-28 06:07:47 +0530

    Question of the day!

---
+++ LeetCodeQuestions/LeetCodeQuestions/GetTarget.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeQuestions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GetTarget
+    {
+        public bool Search(int[] nums, int target)
+        {
+            if (nums.Length == 1 && nums[0] == target)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < nums.Length;)
+            {
+                if (target == nums[i])
+                {
+                    return true;
+                }
+                else if (i < nums.Length && target > nums[i])
+                {
+                    i++;
+                    continue;
+                }
+                else if (target < nums[i])
+                {
+                    while (i < nums.Length - 1 && nums[i + 1] > nums[i])
+                    {
+                        i += 1;
+                    }
+
+                    i += 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
